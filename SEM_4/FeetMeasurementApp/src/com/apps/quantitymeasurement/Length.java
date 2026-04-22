6656219f54f8e521f026fb0ec5348c365fef489d
itzayush18 <itzayushh18@gmail.com>
2026-04-22 16:10:58 +0530

    Completed UC3

---
+++ SEM_4/FeetMeasurementApp/src/com/apps/quantitymeasurement/Length.java
@@ -0,0 +1,41 @@
+package com.apps.quantitymeasurement;
+
+public class Length {
+
+    private final double value;
+    private final LengthUnit unit;
+
+    public enum LengthUnit {
+        FEET(12.0),
+        INCHES(1.0);
+
+        private final double conversionFactor;
+
+        LengthUnit(double conversionFactor) {
+            this.conversionFactor = conversionFactor;
+        }
+
+        public double getConversionFactor() {
+            return conversionFactor;
+        }
+    }
+
+    public Length(double value, LengthUnit unit) {
+        if (unit == null) throw new IllegalArgumentException("Unit cannot be null");
+        this.value = value;
+        this.unit = unit;
+    }
+
+    private double toBaseUnit() {
+        return this.value * this.unit.getConversionFactor();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Length other = (Length) obj;
+        return Double.compare(this.toBaseUnit(), other.toBaseUnit()) == 0;
+    }
+}
